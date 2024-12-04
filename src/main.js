41d35fe4b73f057b91dd3162752d4cf88858b530
Mihir K <mihir@soulhq.ai>
2024-12-04 17:55:18 +0530

    modified a few to learn

--- src/main.js
+++ src/main.js
@@ -1,24 +1,24 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+import { setupCounter } from "./counter.js";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+document.querySelector("#app").innerHTML = /*html*/ `
+  <div class="sidebar">
+   sidebar
   </div>
-`
+  <div class="main">
+    <div class="head1">
+      head
+    </div>
+    <div class="parts">
+      parts
+    </div>
+    <div class="mid">
+      mid
+    </div>
+    <div class="bottom_nav">
+      navigate
+    </div>
+  </div>
+`;
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(document.querySelector("#counter"));
