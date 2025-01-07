615783bdb14e8f8379e0e34885be3907bf250ee1
Mihir K <mihir@soulhq.ai>
2025-01-07 13:06:07 +0530

    final edits to preserve what i made

--- src/main.js
+++ src/main.js
@@ -1,24 +1,44 @@
-import "./style.css";
+// import "./style.css";
 import { setupCounter } from "./counter.js";
 
+
 document.querySelector("#app").innerHTML = /*html*/ `
-  <div class="sidebar">
-   sidebar
+  <div class="head1">
+    <h1 class="headline1"> Heading </h1>
   </div>
   <div class="main">
-    <div class="head1">
-      head
+    <div id="sidebar" class="sidebar">
+      Sidebar
     </div>
-    <div class="parts">
-      parts
-    </div>
-    <div class="mid">
-      mid
-    </div>
-    <div class="bottom_nav">
-      navigate
+    <div class="content">
+      <div class="top">
+        <button id="side" class="side_button"><img src="/src/images/sidebar.svg" class="icon"></button>
+        <div class="menu">
+          <span>|</span>
+          <a class="menu_element" href="/src/html_pages/form.html"> place 1 </a>
+          <span>| |</span>
+          <a class="menu_element" href="/src/html_pages/form-css.html"> place 2 </a>
+          <span>| |</span>
+          <a class="menu_element" href="https://google.com"> place 3 </a>
+          <span>|</span>
+        </div>
+      </div>
+      <div class="main_content">
+        Content
+      </div>
+      <div class="bottom_nav">
+        Navigate
+      </div>
     </div>
   </div>
+
 `;
+
+const but = document.querySelector("#side");
+const sde = document.querySelector("#sidebar");
+but.onclick = function() {
+  sde.classList.toggle("is-hidden");
+}
+
 
 setupCounter(document.querySelector("#counter"));
